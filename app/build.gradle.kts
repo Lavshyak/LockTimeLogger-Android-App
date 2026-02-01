8ab16a11031fa8218d522e562d36e9eedf0d66c5
Lavshyak <73601154+Lavshyak@users.noreply.github.com>
2026-02-01 20:23:50 +0500

    versioncode = 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.example.locktimelogger"
         minSdk = 33
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
